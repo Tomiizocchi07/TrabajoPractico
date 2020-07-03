4c13be340df8d0172ea64743ab70d644a19c5598
ezebenza2000 <ebenzaquen@fi.uba.ar>
2020-07-02 22:11:09 -0300

    Le cambie a pruebaArchivo.cpp para ver como se manejaria con objetos

--- pruebaArchivo.cpp
+++ pruebaArchivo.cpp
@@ -1,82 +1,87 @@
+
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <sstream>
+#include "Nodo.h"
+#include "Lista.h"
+#include "Peliculas.h"
+
 using namespace std;
 
 
 
 int main(){
 
-  ifstream archivo;
-  archivo.open("prueba.txt", ios::in);
-
-  if (archivo.fail()){
-    cout << "El archivo no se abrio" << endl;
-  }
-
-  string nombre;
-
-  string mystr;
-
-  int contador = 0;
-
-  while(!archivo.eof()){
-    getline(archivo,mystr);
 
 
-    if (contador == 0){
-      nombre = mystr;
-      cout << "Nombre Pelicula= " << nombre << endl;
-      contador++;
+    ifstream archivo;
+    archivo.open("prueba.txt", ios::in);
+
+    if (archivo.fail()){
+        cout << "El archivo no se abrio" << endl;
     }
+    Lista<Pelicula>* pLista;
 
-    else if (contador == 1){
-      nombre = mystr;
-      cout << "Genero = " << nombre << endl;
-      contador ++;
-    }
+    string nombrePelicula, nombreDirector, genero, actor;
+    int puntaje;
+    string mystr;
+    Lista<string>* pAux = 0;
+    int contador = 0;
 
-    else if ( contador == 2 ){
-      nombre = mystr;
-      cout << "Puntaje = " << nombre << endl;
-      contador++;
-    }
+    while(!archivo.eof()){
+        Lista<string> actores;
+        getline(archivo,mystr);
 
-    else if ( contador == 3 ){
-      nombre = mystr;
-      cout << "Director = " << nombre << endl;
-      contador ++;
-    }
-
-    else if (contador == 4){
-      cout << "nombre actores:" <<endl;
-      int marcador = 0;
-
-      for ( unsigned int i = 0; i <= mystr.length(); i++){
-
-        if (mystr[i] == '_'){
-          mystr[i] = ' ';
+        if (contador == 0){
+            nombrePelicula = mystr;
+            contador++;
         }
 
-        else if (mystr[i] == ' ' || (i == mystr.length() && mystr[i] != ' ')){
-          nombre = mystr.substr(marcador,i-marcador);
-          marcador = i+1;
-          cout << nombre << endl;
-
-
+        else if (contador == 1){
+            genero = mystr;
+            contador ++;
         }
 
-      }
-      contador++;
+        else if ( contador == 2 ){
+            puntaje = stringstream(mystr);
+            contador++;
+        }
+
+        else if ( contador == 3 ){
+            nombreDirector = mystr;
+            contador ++;
+        }
+
+        else if (contador == 4){
+            cout << "nombre actores:" <<endl;
+            int marcador = 0;
+
+            for ( unsigned int i = 0; i <= mystr.length(); i++){
+
+                if (mystr[i] == '_'){
+                    mystr[i] = ' ';
+                }
+
+                else if (mystr[i] == ' ' || (i == mystr.length() && mystr[i] != ' ')){
+                    actor = mystr.substr(marcador,i-marcador);
+                    marcador = i+1;
+                }
+                actores.insert(actor);
+                pAux = &actores;
+            }
+        contador++;
+        }
+
+        else if (contador == 5){
+            Peliculas pelicula(nombrePelicula,generoPelicula,puntajePelicula,directorPelicula);
+            pelicula.setActores(pAux);
+            (*pLista).insert(pelicula);
+            contador = 0;
+        }
+
     }
+    archivo.close();
 
-    else if (contador == 5){
-      cout<<mystr<<endl;
-      contador = 0;
-    }
-
-  }
-  archivo.close();
-
-  return 0;
+    return 0;
 }
