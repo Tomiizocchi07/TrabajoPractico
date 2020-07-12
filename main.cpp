949b818cbadde7a3fc1c19157c0feba197d92c6b
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-12 20:26:45 -0300

    Modificaciones para la interfaz, agregue lista recomendadas

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 #include "Peliculas.h"
 #include "archivo.h"
 
+const string mensaje = "[-] El archivo de peliculas NO vistas no se pudo abrir correctamente. Por favor verifique que el archivo exista.";
+
 int main()
 {
     Lista<Peliculas> lista_vistas;
     Lista<Peliculas> lista_no_vistas;
     Lista<Peliculas> lista_recomendadas;
+    
     Lista<Peliculas> *l_vistas = &lista_vistas;
     Lista<Peliculas> *l_no_vistas = &lista_no_vistas;
     Lista<Peliculas> *l_recomendadas = &lista_recomendadas;
+    
     Archivo archivos;
-    archivos.leerArchivos("peliculas_vistas.txt", "peliculas_no_vistas.txt", l_vistas, l_no_vistas);
-    Menu menu(l_vistas,l_no_vistas,l_recomendadas);
-    menu.mostrar_menu();
+    if(archivos.leerArchivos("peliculas_vistas.txt", "peliculas_no_vistas.txt", l_vistas, l_no_vistas))
+    {
+        archivos.cargarListaRecomendadas(l_vistas, l_no_vistas, l_recomendadas);
+        Menu menu(l_vistas,l_no_vistas,l_recomendadas);
+        menu.mostrar_menu();
+    }
+    else
+    {
+        cout << mensaje << endl;
+    }
     return (0);
 }
