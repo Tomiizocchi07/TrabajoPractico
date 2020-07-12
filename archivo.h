8e0981c351a232053edfa3403df69ec8c441b306
agusleon <67339017+agusleon@users.noreply.github.com>
2020-07-12 20:14:23 -0300

    Puse PRES y POST, arregle lista de recomendadas

--- archivo.h
+++ archivo.h
@@ -15,17 +15,27 @@ private:
 public:
     //Constructor
     //PRE: -.
-    //POST: Objeto archivo creado
+    //POST: Objeto archivo creado.
     Archivo(){};
     //Destructor
-    //PRE: Objeto archivo creado
+    //PRE: Objeto archivo creado.
     //Libera recursos, memoria, etc.
     ~Archivo(){};
+    //PRE: Variable archivo de lectura por parametro.
+    //POST: Devuelve booleana depende de si se pudo abrir el archivo o no.
     bool existeArchivo(ifstream& archivo);
+    //PRE: Dos strings y dos punteros a lista de peliculas, una por cada archivo.
+    //POST: Devuelve booleana depende de si pudo leer los archivos o no.
     bool leerArchivos(string nombreArchivoVistas, string nombreArchivoNoVistas, Lista<Peliculas>* pListaVistas, Lista<Peliculas>* pListaNoVistas);
+    //PRE: Archivo de lectura y puntero a la lista de peliculas que se va a cargar.
+    //POST: Lista cargada.
     void cargarLista(ifstream& archivo, Lista<Peliculas>* pLista);
+    //PRE: 3 punteros a lista de peliculas, por cada lista.
+    //POST: Lista de Recomendadas cargada.
     void cargarListaRecomendadas(Lista<Peliculas>* pListaVistas, Lista<Peliculas>* pListaNoVistas, Lista<Peliculas>* pListaRecomendadas);
-    void cargarActores(ifstream& archivo, Peliculas pelicula);
+    //PRE: Dos punteros a lista de strings (actores).
+    //POST: Devuelve booleana depende de si coinciden en algun actor o no.
+    bool coincide_actores(Lista<string>* lista_actores_no_vistas, Lista<string>* lista_actores_vistas);
 };
 
 #endif /* archivo_hpp */
